3b03f3f4c33556f4d1ee6e3f4670912126bfa8d3
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-01-22 23:41:33 +0530

    temp component folder

---
+++ app/Components/Nav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <h1>Temp nav</h1>
+    </div>
+  );
+};
+
+export default Nav;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Nav from "./Components/Nav";
+
 export default function Home() {
   return (
     <div>
+      <Nav />
       <h1>Let's begin</h1>
     </div>
   );
