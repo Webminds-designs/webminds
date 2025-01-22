430f240e204eae7a38b00cacdbdefec3ac497502
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-01-23 00:50:58 +0530

    feat/static navBar 	modified:   app/Components/Nav.tsx 	new file:   app/font.css 	modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   public/Fonts/FunnelDisplay-Bold.ttf 	new file:   public/Fonts/FunnelDisplay-ExtraBold.ttf 	new file:   public/Fonts/FunnelDisplay-Light.ttf 	new file:   public/Fonts/FunnelDisplay-Medium.ttf 	new file:   public/Fonts/FunnelDisplay-Regular.ttf 	new file:   public/Fonts/FunnelDisplay-SemiBold.ttf 	modified:   tailwind.config.ts

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -1,9 +1,51 @@
+import Link from "next/link";
 import React from "react";
 
 const Nav = () => {
   return (
-    <div>
-      <h1>Temp nav</h1>
+    <div className="w-full bg-transparent font-FunnelDisplayRegular flex justify-between items-center px-20 py-12">
+      {/* Logo Section */}
+      <div className="text-xl font-extrabold tracking-wide text-white">
+        WebMinds
+      </div>
+
+      {/* Navigation Links */}
+      <nav className="flex ">
+        <ul className="flex items-center space-x-16 text-sm font-normal text-white">
+          <li>
+            <Link
+              href="/"
+              className="hover:text-blue-500 transition duration-200"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/works"
+              className="hover:text-blue-500 transition duration-200"
+            >
+              Works
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/expertise"
+              className="hover:text-blue-500 transition duration-200"
+            >
+              Experties
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/career"
+              className="hover:text-blue-500 transition duration-200"
+            >
+              Carrer
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        FunnelDisplayRegular: ["FunnelDisplay-Regular", "sans-serif"], // Define your custom font
+      },
     },
   },
   plugins: [],
