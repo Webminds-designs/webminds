0a5ba066f7a84254041e9aef3bc15d737b3fb801
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-01-23 19:17:52 +0530

    feat/experties dropdown in nav

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -9,9 +9,12 @@ import logo from "../../public/assets/WebMinds Logo_WebMinds Blue.png";
 
 import { HiMenuAlt1 } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
+import { FaArrowDown } from "react-icons/fa6";
 
 const Nav = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isExpertiesHovered, setExpertiesHovered] = useState(false);
+  const [Hovered, setHovered] = useState("marketing");
   const menuRef = useRef(null);
 
   const handleMouseEnter = (event) => {
@@ -47,6 +50,32 @@ const Nav = () => {
     }
   };
 
+  const toggleCloseDrop = () => {
+    if (isExpertiesHovered) {
+      // Closing the dropdown
+      gsap.to(menuRef.current, {
+        scaleY: 0, // Scale down to 0
+        duration: 0.5,
+        ease: "power2.inOut",
+        onComplete: () => setExpertiesHovered(false), // Set state after animation
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (isExpertiesHovered) {
+      // Opening the dropdown
+      gsap.fromTo(
+        menuRef.current,
+        { scaleY: 0, transformOrigin: "top center" }, // Start from scaleY 0
+        { scaleY: 1, duration: 0.5, ease: "power2.inOut" } // Scale to full height
+      );
+    }
+  }, [isExpertiesHovered]);
+  const hoverHandel = (link: string) => {
+    setHovered(link);
+  };
+
   useEffect(() => {
     if (isMobileMenuOpen) {
       // Animate opening
@@ -61,24 +90,32 @@ const Nav = () => {
   return (
     <div className="w-full bg-transparent font-FunnelDisplayRegular flex justify-between items-center px-6 lg:px-20 py-4 relative">
       {/* Logo Section */}
-      <div className="w-24 cursor-pointer z-20">
+      <div className="w-24 cursor-pointer z-30">
         <Image src={logo} alt="logo" />
       </div>
 
       {/* Desktop Navigation Links */}
-      <nav className="hidden lg:flex">
+      <nav className="hidden lg:flex ">
         <ul className="flex items-center space-x-16 text-sm text-white font-semibold">
           {["Home", "Works", "Experties", "Careers", "Contact"].map(
             (item, index) => (
-              <li key={index}>
+              <li key={index} className="group">
                 <Link
                   href={`/${item.toLowerCase()}`}
-                  onMouseEnter={handleMouseEnter}
-                  className="relative inline-block overflow-hidden hover:opacity-50"
-                  style={{ perspective: "1000px", display: "inline-block" }}
+                  onMouseEnter={(e) => {
+                    handleMouseEnter(e);
+                    if (item === "Experties") setExpertiesHovered(true);
+                  }}
+                  onMouseLeave={() => {
+                    if (item === "Experties") {
+                      setExpertiesHovered(false);
+                    }
+                  }}
+                  className="relative overflow-hidden flex items-center hover:opacity-50 gap-2 z-30"
+                  style={{ perspective: "1000px" }}
                 >
                   <span
-                    className="block"
+                    className="block "
                     style={{
                       display: "inline-block",
                       transformOrigin: "center center", // Center the transform origin
@@ -86,7 +123,62 @@ const Nav = () => {
                   >
                     {item}
                   </span>
+                  {item === "Experties" && (
+                    <div className="flex items-center rounded-full bg-gray-700 p-1">
+                      <FaArrowDown className="text-white w-2 h-2 " />
+                    </div>
+                  )}
                 </Link>
+                {item === "Experties" && isExpertiesHovered && (
+                  <div
+                    className="fixed top-0 left-0 w-screen flex items-center justify-between bg-[#212121] shadow-lg opacity-100 transition-opacity duration-300 z-10"
+                    ref={menuRef}
+                    style={{ transformOrigin: "top center", scaleY: 0 }}
+                    onMouseEnter={() => setExpertiesHovered(true)}
+                    onMouseLeave={() => toggleCloseDrop()}
+                  >
+                    <div className="mt-28 px-24 mb-20 w-full flex justify-between">
+                      <div>Our Experties</div>
+                      <ul className="md:text-3xl lg:text-5xl ">
+                        <li
+                          className="px-4 py-3"
+                          onMouseEnter={() => hoverHandel("marketing")}
+                        >
+                          <Link href="/experties/marketing">
+                            Digital Marketing
+                          </Link>
+                        </li>
+                        <li
+                          className="px-4 py-3"
+                          onMouseEnter={() => hoverHandel("web")}
+                        >
+                          <Link href="/experties/development">
+                            Website Development
+                          </Link>
+                        </li>
+                        <li className="px-4 py-3">
+                          <Link href="/experties/socialmedia">
+                            Social Media
+                          </Link>
+                        </li>
+                        <li className="px-4 py-3">
+                          <Link href="/experties/branding">
+                            Branding & Design
+                          </Link>
+                        </li>
+                      </ul>
+                      <div className="w-80 h-80">
+                        {Hovered === "web" ? (
+                          <div className="w-full h-full bg-slate-400"></div>
+                        ) : Hovered === "marketing" ? (
+                          <div className="w-full h-full bg-green-200"></div>
+                        ) : (
+                          <div className="w-full h-full bg-transparent"></div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                )}
               </li>
             )
           )}
