e42100677fc8d4e0ef47293fe1284727eba038fb
LonathG <lk.gcm29@gmail.com>
2025-01-26 21:40:15 +0530

    Minor Bug fixex and added youtube link

--- app/Components/Socials.jsx
+++ app/Components/Socials.jsx
@@ -5,24 +5,35 @@ import MagneticIcons from './MagneticIcons';
 export default function Socials() {
   return (
     <main className={styles.main}>
+
+      <h1 className="text-xl font-bold">Socials</h1>
+
       <div className={styles.container}>
-          <MagneticIcons>
-            <img src="/facebook.svg" alt="Icon 1" />
-          </MagneticIcons>
-          
-          <MagneticIcons>
-            <img src="/instagram.svg" alt="Icon 2" />
-          </MagneticIcons>
-          
-          <MagneticIcons>
-            <img src="/youtube.svg" alt="Icon 4" />
-          </MagneticIcons>
-          
-          <MagneticIcons>
-            <img src="/tik-tok.svg" alt="Icon 3" />
-          </MagneticIcons>
-          
-        </div>
-      </main>
+        <MagneticIcons>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="/facebook.svg" alt="Facebook" />
+          </a>
+        </MagneticIcons>
+        
+        <MagneticIcons>
+          <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+            <img src="/tik-tok.svg" alt="TikTok" />
+          </a>
+        </MagneticIcons>
+
+        <MagneticIcons>
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <img src="/youtube.svg" alt="YouTube" />
+          </a>
+        </MagneticIcons>
+
+        <MagneticIcons>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src="/instagram.svg" alt="Instagram" />
+          </a>
+        </MagneticIcons>
+        
+      </div>
+    </main>
   );
 }
