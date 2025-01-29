6f77ab621115eaf345c4b4a253271762d86a6517
LonathG <lk.gcm29@gmail.com>
2025-01-30 02:37:54 +0530

    Slightly changed the cursor and added a custom cursor to the expertise section

--- app/Components/Expertise.jsx
+++ app/Components/Expertise.jsx
@@ -1,49 +1,54 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useScroll } from "framer-motion";
-import Lenis from '@studio-freight/lenis';
-
+import Lenis from "@studio-freight/lenis";
 import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section5";
+import CustomCursor from "./CustomCursor";
 
 export default function Expertise() {
     const container = useRef(null);
     const { scrollYProgress } = useScroll({
         target: container,
-        offset: ["start start", "end end"]
+        offset: ["start start", "end end"],
     });
 
+    const [isHovering, setIsHovering] = useState(false);
+
     useEffect(() => {
-        if (typeof window === 'undefined') return; // Ensure code runs only on the client
+        if (typeof window === "undefined") return;
 
         const lenis = new Lenis({
             smoothWheel: true,
             smoothTouch: true,
-            lerp: 0.1, // Adjust smoothness
+            lerp: 0.1,
         });
 
-        // Animation loop
         const animate = (time) => {
-            lenis.raf(time); // Use raf to integrate Lenis with requestAnimationFrame
-            requestAnimationFrame(animate); // Recursively call animate
+            lenis.raf(time);
+            requestAnimationFrame(animate);
         };
 
-        // Start the animation loop
         requestAnimationFrame(animate);
 
-        // Cleanup on unmount
         return () => {
             lenis.destroy();
         };
     }, []);
 
     return (
-        <main ref={container} className="relative h-[400vh] bg-[black]">
-            <Section1 scrollYProgress={scrollYProgress} />
-            <Section2 scrollYProgress={scrollYProgress} />
-            <Section3 scrollYProgress={scrollYProgress} />
-            <Section4 scrollYProgress={scrollYProgress} />
+        <main ref={container} className="relative h-[405vh] bg-[black]">
+            <CustomCursor isHovering={isHovering} />
+            <div
+                onMouseEnter={() => setIsHovering(true)}
+                onMouseLeave={() => setIsHovering(false)}
+            >
+                <Section1 scrollYProgress={scrollYProgress} />
+                <Section2 scrollYProgress={scrollYProgress} />
+                <Section3 scrollYProgress={scrollYProgress} />
+                <Section4 scrollYProgress={scrollYProgress} />
+            </div>
         </main>
     );
 }
