4ab5810d1291029b58be2d91952c9c7b0a5228d6
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-02-03 23:10:33 +0530

    fix font loop

--- app/Components/Bigfontloop.tsx
+++ app/Components/Bigfontloop.tsx
@@ -4,7 +4,7 @@ import ParallaxText from "./ParallaxText";
 const Bigfontloop = () => {
   return (
     <>
-      <main className="flex flex-col items-center justify-center h-fit  bg-transparent text-text absolute bottom-2">
+      <main className="flex flex-col items-center justify-center h-fit  bg-transparent text-text absolute bottom-0 ">
         <ParallaxText baseVelocity={-0.5} fontSize="text-[200px]">
           Crafting Beyond Ordinary Minds - Crafting Beyond Ordinary Minds -
           Crafting Beyond Ordinary Minds -
