8d62836586a3f66af9aefc0b47c874d330590a04
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-27 21:10:07 +0530

    refactor: update ESLint configuration to use CommonJS syntax for compatibility
    fix:Cannot serialize key "parse" in parser: Function values are not supported

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+const { FlatCompat } = require("@eslint/eslintrc");
+const { dirname } = require("path");
+const { fileURLToPath } = require("url");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,14 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals"],
-  }),
-];
-
-export default eslintConfig;
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  overrides: compat.overrides,
+};
