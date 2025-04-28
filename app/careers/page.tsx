40f150155a417fb446c120e16d793f4e90472174
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-04-28 23:55:26 +0530

    refactor: simplify CareersPage component by removing unused code and improving layout

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+
+import React from "react";
 import bg from "../asserts/Lbg2.jpg"; // Ensure the path is correct
 import bg2 from "../asserts/careerBg3.png"; // Ensure the path is correct
 import Image from "next/image";
@@ -7,112 +8,19 @@ import Nav from "../Components/Nav";
 import Footer from "../Components/Footer";
 import vacancyData from "./vacancyData";
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
-// import CustomCursor from "../Components/CustomCursor";
-
 import { motion } from "framer-motion";
 
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
-
 const CareersPage = () => {
-  // const [hovering, setHovering] = useState(false);
-
-  const containerRef = useRef(null);
-  const containerRef2 = useRef(null);
-
-  const mainTitleRef = useRef(null);
-  const image1Ref = useRef(null);
-  const image2Ref = useRef(null);
-  const text1Ref = useRef(null);
-  const text2Ref = useRef(null);
-
-  const cardContainerRef = useRef(null);
-
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      // ✅ Make text overlay scroll faster
-      gsap.to(mainTitleRef.current, {
-        y: "-10vh", // Moves text up more
-        ease: "power1.out",
-        scrollTrigger: {
-          trigger: mainTitleRef.current,
-          start: "top 30%",
-          end: "top 40%",
-          scrub: 1, // Moves faster
-        },
-      });
-      gsap.to(image1Ref.current, {
-        y: "30vh", // Moves text up more
-        ease: "power1.out",
-        scrollTrigger: {
-          trigger: image1Ref.current,
-          start: "top 30%",
-          end: "bottom 40%",
-          scrub: 1,
-        },
-      });
-      gsap.to(text1Ref.current, {
-        y: "-10vh", // Moves text up more
-        ease: "power1.out",
-        scrollTrigger: {
-          trigger: text1Ref.current,
-          start: "top 30%",
-          end: "bottom 40%",
-          scrub: 1,
-        },
-      });
-    }, containerRef);
-
-    let ctx2 = gsap.context(() => {
-      gsap.to(image2Ref.current, {
-        y: "60vh", // Moves text up more
-        ease: "power1.out",
-        scrollTrigger: {
-          trigger: image2Ref.current,
-          start: "top top",
-          end: "bottom 40%",
-          scrub: 1,
-        },
-      });
-      gsap.to(text2Ref.current, {
-        y: "-30vh", // Moves text up more
-        ease: "power1.out",
-        scrollTrigger: {
-          trigger: text2Ref.current,
-          start: "bottom bottom",
-          end: "bottom 40%",
-          scrub: 1,
-        },
-      });
-      gsap.to(cardContainerRef.current, {
-        y: "-30vh", // Moves text up more
-        ease: "power1.out",
-        scrollTrigger: {
-          trigger: cardContainerRef.current,
-          start: "bottom bottom",
-          end: "bottom 40%",
-          scrub: 1,
-        },
-      });
-    });
-
-    return () => {
-      ctx.revert();
-      ctx2.revert();
-    };
-  });
+  const title = "Careers";
 
   return (
     <>
       <Nav />
-      {/* <CustomCursor hovering={hovering} /> */}
       <motion.div
         className="relative bg-black md:w-screen h-screen md:h-fit"
-        initial={{ opacity: 1, y: 1000 }}
+        initial={{ opacity: 1, y: "10%" }}
         animate={{ opacity: 1, y: 10 }}
-        transition={{ duration: 1 }}
-        ref={containerRef}
+        transition={{ duration: 0.8 }}
       >
         <Image
           src={bg}
@@ -120,30 +28,34 @@ const CareersPage = () => {
           layout="cover"
           objectFit="fill"
           className="opacity-80 relative -top-40"
-          ref={image1Ref}
         />
 
-        <div className="w-screen h-screen flex justify-center items-start md:items-center absolute top-24 md:top-0">
-          <div
-            className="text-5xl md:text-[100px] lg:text-[250px]  opacity-80 "
-            style={{
-              display: "inline-block",
-              fontFamily: "eight, sans-serif",
-            }}
-            ref={mainTitleRef}
-          >
-            Careers .
+        <div className="w-screen h-screen flex justify-center pt-20 md:pl-16 items-start md:items-start absolute top-24 md:top-0">
+          <div className="text-5xl w-full h-screen md:text-[100px] lg:text-[220px]  font-AlbertSans_Bold text-start mt-20 md:mt-10 text-text flex">
+            {title.split("").map((char, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{
+                  duration: 0.8,
+                  delay: index * 0.1,
+                  ease: "easeOut",
+                }}
+                className="inline-block"
+              >
+                {char}
+              </motion.span>
+            ))}
           </div>
         </div>
-        <div
-          className="w-fit h-fit px-8 md:px-24 text-2xl md:text-4xl lg:text-5xl text-white absolute bottom-0 md:top-[100vh]"
-          ref={text1Ref}
-        >
+
+        <div className="w-fit h-screen px-8 md:px-24 text-2xl md:text-4xl lg:text-5xl text-white  font-AlbertSans_Regular">
           <span
             style={{
               display: "inline-block",
-              fontFamily: "eight, sans-serif",
             }}
+            className="font-AlbertSans_Bold text-blue-500"
           >
             This is WebMinds -
           </span>
@@ -156,29 +68,22 @@ const CareersPage = () => {
           follow trends—we create them.
         </div>
       </motion.div>
-      <div
-        className="relative bg-black md:w-screen h-screen md:h-fit"
-        /// <reference path="" />
-        ref={containerRef2}
-      >
+
+      <div className="relative bg-black md:w-screen h-screen md:h-fit">
         <Image
           src={bg2}
           alt="bg image"
           layout="cover"
           objectFit="fill"
           className="opacity-80 -z-50"
-          ref={image2Ref}
         />
 
-        <div
-          className="w-fit h-fit p-8 md:p-24 text-2xl md:text-4xl lg:text-6xl text-white absolute top-2"
-          ref={text2Ref}
-        >
+        <div className="w-fit h-fit p-8 md:p-24 text-2xl md:text-4xl lg:text-5xl text-white absolute top-8">
           <span
             style={{
               display: "inline-block",
-              fontFamily: "eight, sans-serif",
             }}
+            className="font-AlbertSans_Bold text-blue-500"
           >
             Our Spirit -
           </span>
@@ -188,23 +93,18 @@ const CareersPage = () => {
           we think outside of them. As a team, we aren’t just building
           websites—we’re shaping the digital landscape of tomorrow, today.
         </div>
-        <div
-          className="flex flex-wrap gap-8 justify-center p-4 md:p-24 absolute bottom-0 md:bottom-60"
-          ref={cardContainerRef}
-        >
+
+        <div className="flex flex-wrap gap-8 justify-center p-4 md:p-24 absolute md:bottom-0  ">
           {vacancyData.map((vacancy) => (
             <div
               key={vacancy.id}
               className="md:w-[420px] w-[200px] border-2 border-slate-500 flex flex-col items-start justify-between gap-2 md:gap-6 p-2 md:p-8 rounded-lg text-text 
-           transition-colors duration-700 ease-in-out hover:bg-slate-500 hover:text-white"
-              // onMouseEnter={() => setHovering(true)}
-              // onMouseLeave={() => setHovering(false)}
+              transition-colors duration-700 ease-in-out hover:bg-slate-500 hover:text-white"
             >
               <h2
-                className="text-2xl md:text-4xl lg:text-6xl text-wrap text-start font-bold"
+                className="text-2xl md:text-4xl lg:text-6xl text-wrap text-start font-light font-AlbertSans_Bold"
                 style={{
                   display: "inline-block",
-                  fontFamily: "eight, sans-serif",
                 }}
               >
                 {vacancy.title}
