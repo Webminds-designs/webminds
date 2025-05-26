918c18a556d7c668598bee85ab69da3e059a74ba
root <root@srv666175.hstgr.cloud>
2025-05-26 15:34:08 +0000

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: "standalone",
+  generateBuildId: () => {
+    return `${Date.now()}`; // for chunk cache busting
+  },
+  images: {
+    domains: ["www.webmindsdesigns.com"],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "1mb",
+      allowedOrigins: ["https://www.webmindsdesigns.com"],
+    },
+  },
 };
 
 export default nextConfig;
