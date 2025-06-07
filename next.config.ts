c239b29753dfd6cce86fae8cf071a1cc07186b7a
rumeshsmrr <siriwardhanarumesh@gmail.com>
2025-06-07 15:25:21 +0530

    Refactor error handling in ClientLayout and layout components; remove unused bundle analyzer configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,15 @@
 import type { NextConfig } from "next";
-import withBundleAnalyzer from "@next/bundle-analyzer";
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  compress: true,
-  poweredByHeader: false,
-  generateBuildId: () => `${Date.now()}`, // Chunk cache busting
+  generateBuildId: () => {
+    return `${Date.now()}`; // for chunk cache busting
+  },
   images: {
     domains: ["www.webmindsdesigns.com"],
   },
   experimental: {
-    // ⚠️ Only enable this if you use serverActions
     serverActions: {
       bodySizeLimit: "1mb",
       allowedOrigins: ["https://www.webmindsdesigns.com"],
@@ -23,4 +17,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default bundleAnalyzer(nextConfig);
+export default nextConfig;
