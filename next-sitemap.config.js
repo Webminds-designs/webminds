49df6333d9030f6793010df1f4b8b1436d215e93
root <root@srv666175.hstgr.cloud>
2025-06-09 18:48:30 +0000

    Update siteUrl in next-sitemap.config.js to reflect the correct domain

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.webminds.com",
+  siteUrl: "https://webmindsdesigns.com/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
